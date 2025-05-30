c4987e31598842cf118f1a51ea681e99e19b6637
Luiz Braz <brazluiz1000@gmail.com>
2025-05-29 23:19:04 -0300

    Melhorias no Accordion

--- script/script.js
+++ script/script.js
@@ -1,39 +1,36 @@
+function navegacaoTab() {
+  const jsTab = document.querySelectorAll(".js-tab li");
+  const jsContent = document.querySelectorAll(".js-content div");
 
-function navegacaoTab () {
-const jsTab = document.querySelectorAll(".js-tab li");
-const jsContent = document.querySelectorAll(".js-content div");
-
-if (jsTab.length && jsContent.length) {
-  jsContent[0].classList.add("mostrar");
-  jsTab.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      jsContent.forEach((item) => item.classList.remove("mostrar"));
-      jsContent[index].classList.add("mostrar");
+  if (jsTab.length && jsContent.length) {
+    jsContent[0].classList.add("mostrar");
+    jsTab.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        jsContent.forEach((item) => item.classList.remove("mostrar"));
+        jsContent[index].classList.add("mostrar");
+      });
     });
-  });
+  }
 }
-
-}
-
 
 navegacaoTab();
 
+function navegacaoAccordion() {
+  const dtFaq = document.querySelectorAll(".faq dt");
+  const ddFaq = document.querySelectorAll(".faq dd");
 
-const dtFaq = document.querySelectorAll('.faq dt')
-const ddFaq = document.querySelectorAll('.faq dd')
+  if (dtFaq.length && ddFaq.length) {
+    ddFaq[0].classList.add("mostrar");
+    dtFaq[0].classList.add("mostrar");
 
+    dtFaq.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.currentTarget.classList.toggle("mostrar");
 
-if (dtFaq.length && ddFaq.length) {
-ddFaq[0].classList.add('mostrar')
-dtFaq[0].classList.add('mostrar')
-
-dtFaq.forEach( (item) => 
-{      
-
-        item.addEventListener('click', (e) => {
-          e.currentTarget.classList.toggle('mostrar')
-        
-          e.currentTarget.nextElementSibling.classList.toggle('mostrar')
-        })
-})
+        e.currentTarget.nextElementSibling.classList.toggle("mostrar");
+      });
+    });
+  }
 }
+
+navegacaoAccordion();
