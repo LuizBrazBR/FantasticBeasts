d437e2a548f27929aaaf5e72b7d6a28262479454
Luiz Braz <brazluiz1000@gmail.com>
2025-06-23 01:39:04 -0300

    Tooltip

--- script/modules/tooltip.js
+++ script/modules/tooltip.js
@@ -1,11 +1,26 @@
 export default function tooltip() {
   const tooltip = document.querySelectorAll("[data-tooltip]");
+  const tooltipElement = document.createElement("div");
+  const ariaLabel = document.querySelector(".contato-mapa img");
 
   function handleOver(e) {
-    console.log(e);
+    tooltipElement.innerText = ariaLabel.getAttribute("aria-label");
+    document.body.appendChild(tooltipElement);
+    tooltipElement.classList.add("tooltip");
+    e.target.addEventListener("mousemove", handleMove);
+  }
+
+  function handleLeave() {
+    tooltipElement.remove();
+  }
+
+  function handleMove(e) {
+    tooltipElement.style.top = e.pageY + 15 + "px";
+    tooltipElement.style.left = e.pageX + 15 + "px";
   }
 
   tooltip.forEach((i) => {
     i.addEventListener("mouseover", handleOver);
+    i.addEventListener("mouseleave", handleLeave);
   });
 }
