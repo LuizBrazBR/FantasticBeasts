475ac2e35ba55b0835e9af865043122cdacf2363
Luiz Braz <brazluiz1000@gmail.com>
2025-07-16 01:19:35 -0300

    Finalizando alteracoes do menu

--- script/modules/menu.js
+++ script/modules/menu.js
@@ -1,13 +1,25 @@
 export default function menu() {
   const menus = document.querySelectorAll("[data-menu] a");
   const ul = document.querySelector("[data-menu] ul");
+  const html = document.documentElement;
 
   menus.forEach((menu) => {
     ["click", "touchstart"].forEach((userEvent) => {
       menu.addEventListener(userEvent, (e) => {
         e.preventDefault();
-        ul.classList.toggle("menuClick");
+        ul.classList.add("menuClick");
+        const target = e.target;
+
+        html.addEventListener("click", (event) =>
+          handleClickOutside(event, target)
+        );
       });
     });
   });
+
+  function handleClickOutside(event, target) {
+    if (!target.contains(event.target)) {
+      ul.classList.remove("menuClick");
+    }
+  }
 }
