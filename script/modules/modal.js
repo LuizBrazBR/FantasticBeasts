7f61dd98dd94b4b551d4003bd073ca9565287ac6
Luiz Braz <brazluiz1000@gmail.com>
2025-11-02 01:57:34 -0300

    Inicio da Refatoracao

--- script/modules/modal.js
+++ script/modules/modal.js
@@ -3,15 +3,15 @@ export default function modal() {
   const modal = document.querySelector('[data-modal="modal"]');
   const fecharModal = document.querySelector('[data-modal="fechar"]');
 
-  if (botaoModal && modal && fecharModal) {
-    function toggleModal(e) {
-      e.preventDefault();
-      modal.classList.toggle("ativo");
-    }
-    function fechar(e) {
-      if (this === e.target) toggleModal(e);
-    }
+  function toggleModal(e) {
+    e.preventDefault();
+    modal.classList.toggle("ativo");
+  }
+  function fechar(e) {
+    if (this === e.target) toggleModal(e);
+  }
 
+  if (botaoModal && modal && fecharModal) {
     botaoModal.addEventListener("click", toggleModal);
     fecharModal.addEventListener("click", toggleModal);
     modal.addEventListener("click", fechar);
--- script/modules/scrollefeito.js
+++ script/modules/scrollefeito.js
@@ -1,16 +1,17 @@
 export default function efeitoScroll() {
   const jsScroll = document.querySelectorAll("h1[data-anime='top']");
+
+  function verificaTop() {
+    jsScroll.forEach((e) => {
+      const top = e.getBoundingClientRect().top;
+      const topWindow = window.innerHeight * 0.6;
+      if (top < topWindow) {
+        e.classList.add("ativo");
+        e.nextElementSibling.classList.add("ativo");
+      }
+    });
+  }
   if (jsScroll.length) {
-    function verificaTop() {
-      jsScroll.forEach((e) => {
-        const top = e.getBoundingClientRect().top;
-        const topWindow = window.innerHeight * 0.6;
-        if (top < topWindow) {
-          e.classList.add("ativo");
-          e.nextElementSibling.classList.add("ativo");
-        }
-      });
-    }
     window.addEventListener("scroll", verificaTop);
     verificaTop();
   }
